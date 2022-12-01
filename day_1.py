cc6f3a2efde1dd946b2d11681b5e431361756ce9
Jakub Wiańczyk <jwianczyk@gmail.com>
2022-12-01 11:49:10 +0100

    Day 1 of Code of Advent parts 1 & 2

---
+++ day_1.py
@@ -0,0 +1,17 @@
+total = 0
+previous_elf = []
+with open('input.txt', 'r') as file:
+    for line in file:
+        try:
+            num = int(line)
+            total += num
+            # print(f"Number:", num)
+        except ValueError:
+            previous_elf.append(total)
+            total = 0
+            # print("-" * 20)
+previous_elf.sort()
+top_elf = previous_elf[-1]
+top_3_elfs = sum(previous_elf[-3:])
+print("Elf with the most value:", (top_elf))
+print("Sum of values for top 3 elfs:", (top_3_elfs))
